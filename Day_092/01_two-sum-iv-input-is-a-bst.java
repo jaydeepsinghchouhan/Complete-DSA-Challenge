06fdb1829a7d35b570a64f9a2510b15607c2dd6a
Jaydeep Singh Chouhan <jaydeepchouhan20808@acropolis.in>
2022-10-09 07:50:06 +0530

    Completed Day 92

---
+++ Day_092/01_two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    Set<Integer> allNodes = new HashSet<>();
+    
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false;
+                
+        if(allNodes.contains(k - root.val)) return true;
+        
+        allNodes.add(root.val);
+        
+        return findTarget(root.left, k) || findTarget(root.right, k);
+        
+    }
+}
